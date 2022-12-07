5d60e3f03281d8340776cd0419267b8b8682c4f0
Mitya <partyzan327@gmail.com>
2022-12-07 21:37:47 +0300

    feat: implement a user greeting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('hello');
+import readline from 'node:readline';
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+const username = process.argv.slice(2)[0].split('=')[1] || 'anonymous';
+
+process.stdout.write(`Welcome to the File Manager, ${username}!` + '\n');
+
+rl.on('line', (answer) => {
+  if (answer === '.exit') {
+    rl.close();
+  }
+});
+
+rl.on('SIGINT', (answer) => {
+  rl.close();
+});
+
+rl.on('close', () => {
+  process.stdout.write(
+    `Thank you for using File Manager, ${username}, goodbye!` + '\n'
+  );
+});
