cc333671b499f92283442c630cdf6d0c13d6f75e
Mitya <partyzan327@gmail.com>
2022-12-08 23:32:26 +0300

    fix: typo

--- src/utils/helpers/printCat.js
+++ src/utils/helpers/printCat.js
@@ -2,19 +2,20 @@ import fs from 'node:fs';
 import { enterAnotherCommand } from './enterAnotherCommand.js';
 import { snowCurrentDir } from './snowCurrentDir.js';
 
-
 export const printCat = (__dirname) => {
   fs.readdir(new URL(__dirname), { withFileTypes: true }, (err, files) => {
     if (err) enterAnotherCommand();
     else {
-      const test = files
+      const catalog = files
         .sort((a) => (a.isDirectory() ? -1 : 1))
         .map((file) => ({
           Name: file.name,
           Type: !file.isDirectory() ? 'file' : 'directory',
         }));
+
       process.stdout.write('\n');
-      console.table(test);
+      console.table(catalog);
+
       snowCurrentDir();
     }
   });
